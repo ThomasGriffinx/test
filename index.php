1a3bcbe82b0e9a33196eab5df55d4e6ff02d7586
thomasgriffinx <xytomg@gmail.com>
2016-07-28 09:40:54 +0100

    master

---
+++ index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
+  <title>BUIT EuCan Dashboard Porfolio Site</title>
+
+  <!-- CSS  -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+</head>
+<body>
+  <nav class="white" role="navigation">
+    <div class="nav-wrapper container">
+      <a id="logo-container" href="#" class="brand-logo">EuCan</a>
+      <ul class="right hide-on-med-and-down">
+        <li><a href="#">Data Reporting</a></li>
+      </ul>
+
+      <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">Menu</i></a>
+    </div>
+  </nav>
+
+  <div id="index-banner" class="parallax-container">
+    <div class="section no-pad-bot">
+      <div class="container">
+        <br><br>
+        <h1 class="header center teal-text text-lighten-2">BUIT EuCan Data Insights</h1>
+        <div class="row center">
+          <h5 class="header col s12 light">Finance Visuals</h5>
+        </div>
+        <div class="row center">
+          <a href="/dashboard.php" id="download-button" class="btn-large waves-effect waves-light teal lighten-1">View Dashboards</a>
+        </div>
+        <br><br>
+
+      </div>
+    </div>
+    <div class="parallax"><img src="background1.jpg" alt="Unsplashed background img 1"></div>
+  </div>
+
+
+  <div class="container">
+    <div class="section">
+
+      <!--   Icon Section   -->
+      <div class="row">
+        <div class="col s12 m4">
+          <div class="icon-block">
+            <h2 class="center brown-text"><i class="material-icons">flash_on</i></h2>
+            <h5 class="center">Visuals</h5>
+
+            <p class="light">Making use of Heroku, Tableau and Force we have provided a modern visualisation solution to organising data from multiple data sources</p>
+          </div>
+        </div>
+
+        <div class="col s12 m4">
+          <div class="icon-block">
+            <h2 class="center brown-text"><i class="material-icons">group</i></h2>
+            <h5 class="center">User Experience Focused</h5>
+
+            <p class="light">By utilizing elements and principles of the modern IT ecosystem, we were able to combine multiple data sets from old and new systems to create tableau visuals, we have hosted them here</p>
+          </div>
+        </div>
+
+        <div class="col s12 m4">
+          <div class="icon-block">
+            <h2 class="center brown-text"><i class="material-icons">settings</i></h2>
+            <h5 class="center">Easy to work with</h5>
+
+            <p class="light">By loading Excel spreadsheets and data extracts from force into our prebuilt tableau dashboards, your visuals will update themselves.</p>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+
+  <div class="parallax-container valign-wrapper">
+    <div class="section no-pad-bot">
+      <div class="container">
+        <div class="row center">
+          <h5 class="header col s12 light">Providing Tableau data visuals</h5>
+        </div>
+      </div>
+    </div>
+    <div class="parallax"><img src="background2.jpg" alt="Unsplashed background img 2"></div>
+  </div>
+
+
+
+
+  <!--  Scripts-->
+  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script src="js/materialize.js"></script>
+  <script src="js/init.js"></script>
+
+  </body>
+
+
+  <?php
+# This function reads your DATABASE_URL configuration automatically set by Heroku
+# the return value is a string that will work with pg_connect
+ // $db = pg_connect('dbname=d81a53c7aurso7 host=ec2-54-228-219-2.eu-west-1.compute.amazonaws.com port=5432 user=qgljwvgpvqdmos password=4WgnfX2SVmyfod0LzIrLzQMh9C sslmode=require');
+
+   
+        ?> 
+</html>
