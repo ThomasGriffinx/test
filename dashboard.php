d736974b187085b4d7742b408459d914d4297c88
Tom Griffin <idanteusa@gmail.com>
2016-08-04 11:54:41 +0100

    update

--- dashboard.php
+++ dashboard.php
@@ -22,8 +22,7 @@
 </div>
 </nav>
 
-<iframe frameborder="0" marginheight="0" marginwidth="0" allowtransparency="true" class="tableauViz" width="1004" height="769" style="display: block; margin: 0px; padding: 0px; border: none; width: 1004px; height: 769px;" src="https://public.tableau.com/views/SPReturns_92/Economy?:embed=y&amp;:showVizHome=no&amp;:host_url=https%3A%2F%2Fpublic.tableau.com%2F&amp;:tabs=no&amp;:toolbar=yes&amp;:animate_transition=yes&amp;:display_static_image=no&amp;:display_spinner=no&amp;:display_overlay=yes&amp;:display_count=yes&amp;:showVizHome=no&amp;:showTabs=y&amp;:loadOrderID=0"></iframe>
-
+<iframe frameborder="0" marginheight="0" marginwidth="0" allowtransparency="true" class="tableauViz" width="1004" height="769" style="display: block; margin: 0px; padding: 0px; border: none; width: 1004px; height: 769px;" src="https://yo2cypress01.ema.lilly.com/javascripts/api/viz_v1.js:embed=y&amp;:showVizHome=no&amp;:host_url=https%3A%2F%2Fpublic.tableau.com%2F&amp;:tabs=no&amp;:toolbar=yes&amp;:animate_transition=yes&amp;:display_static_image=no&amp;:display_spinner=no&amp;:display_overlay=yes&amp;:display_count=yes&amp;:showVizHome=no&amp;:showTabs=y&amp;:loadOrderID=0"></iframe>
 
 
 
